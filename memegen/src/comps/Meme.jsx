47ba300728a49f46ea2f7ffb5fe7c7b0ccc9316b
JagMax <gewoonmaks@gmail.com>
2022-08-04 17:09:30 +0300

    set image

--- memegen/src/comps/Meme.jsx
+++ memegen/src/comps/Meme.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
+import memesData from "../memesData";
+
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: null,
+  });
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  const handleClick = () => {
+    const newImgURL =
+      allMemeImages.data.memes[
+        Math.floor(Math.random() * allMemeImages.data.memes.length)
+      ].url;
+
+    setMeme((prevMeme) => ({ ...prevMeme, randomImage: newImgURL }));
+  };
+
   return (
     <main>
-      <form>
+      <div className="form">
         <div className="inputs">
           <input type="text" placeholder="Upper phrase" />
           <input type="text" placeholder="Lower phrase" />
         </div>
 
-        <button>Get a new meme image</button>
-      </form>
+        <button onClick={handleClick}>Get a new meme image</button>
+      </div>
+
+      <div className="img-container">
+        {meme.randomImage ? (
+          <img className="memeImg" src={meme.randomImage} alt="The Meme" />
+        ) : (
+          "Waiting for someone to meme..."
+        )}
+      </div>
     </main>
   );
 }
